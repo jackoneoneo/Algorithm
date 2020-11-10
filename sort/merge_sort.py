4fbcd5341b75bf93b17df27b4f20cd8027f5dc91
18664385831 <zengzhipeng1208@163.com>
2020-11-10 11:38:42 +0800

    归并排序

---
+++ sort/merge_sort.py
@@ -0,0 +1,39 @@
+'''
+归并排序
+时间复杂度:O(nlgn)
+使用的是分治的思维
+'''
+import  numpy as np
+def merge(a, b):
+    c = []
+    h = j = 0
+    while j < len(a) and h < len(b):
+        if a[j] < b[h]:
+            c.append(a[j])
+            j += 1
+        else:
+            c.append(b[h])
+            h += 1
+
+    if j == len(a):
+        for i in b[h:]:
+            c.append(i)
+    else:
+        for i in a[j:]:
+            c.append(i)
+    return c
+
+
+def merge_sort(lists):
+    if len(lists) <= 1:
+        return lists
+    middle = len(lists) // 2
+    left = merge_sort(lists[:middle])
+    right = merge_sort(lists[middle:])
+    return merge(left, right)
+
+
+if __name__ == '__main__':
+    arr = np.random.randint(1, 1000, size=100)
+    print(arr)
+    print(merge_sort(arr))
